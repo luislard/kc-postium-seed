a0126c8749cb349a80c3ff4511d1726fbd9491ec
Luis Rosales <eng.luisrosales@gmail.com>
2017-10-14 23:01:01 +0200

    suscribing the header to the search event and changing the route

--- src/app/header-bar/header-bar.component.ts
+++ src/app/header-bar/header-bar.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-bar',
   templateUrl: './header-bar.component.html',
   styleUrls: ['./header-bar.component.css']
 })
-export class HeaderBarComponent { }
+export class HeaderBarComponent { 
+
+  constructor(
+    private router: Router
+   ) {}
+
+  changeRouteToPostListWithQueryParams(textToSearch){
+    console.log('aqui estoy en el changeRoute');
+    this.router.navigate(['/posts'], { queryParams: { q: textToSearch } });
+  }
+
+}
